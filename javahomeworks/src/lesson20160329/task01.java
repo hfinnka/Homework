f2ec2312a24bb3d5e5092d12880c38df70987345
hfinnka <hfinnka@gmail.com>
2016-03-31 13:51:50 +0300

    final

--- javahomeworks/src/lesson20160329/task01.java
+++ javahomeworks/src/lesson20160329/task01.java
@@ -3,7 +3,7 @@ package lesson20160329;
 public class task01 {
 
 	public static void main(String[] args) {
-		System.out.println(1 + 3 + 5 + 7 + 9 + 2 + 4 + 6 + 8 + 10);
+		System.out.println(0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
 
 
 	}
