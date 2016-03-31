abb35e4475b5b0615ab80581c17d733b40f6c712
hfinnka <hfinnka@gmail.com>
2016-03-31 13:45:41 +0300

    ok

---
+++ javahomeworks/src/lesson20160329/hello.java
@@ -0,0 +1,9 @@
+package lesson20160329;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+
+}
---
+++ javahomeworks/src/lesson20160329/task01.java
@@ -0,0 +1,11 @@
+package lesson20160329;
+
+public class task01 {
+
+	public static void main(String[] args) {
+		System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+
+
+	}
+
+}
